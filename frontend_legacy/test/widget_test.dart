4d87efff0deabd2e53332018cb363637681fc885
Bahram Mohmand <bahram.mohmand123@gmail.com>
2026-02-15 03:31:49 -0800

    Remove old files

--- frontend_legacy/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('NexusApp smoke test', (WidgetTester tester) async {
-    // Basic smoke test placeholder.
-    // Full widget test requires Supabase mock setup.
-    expect(true, isTrue);
-  });
-}
